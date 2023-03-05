340901e514a00748d7f7dae8e9ff3053bd6b280a
sakkarin2000 <sakkarin.kmutt@mail.kmutt.ac.th>
2023-03-05 23:35:24 +0700

    Merge getTicket to be single endpoint with optional status query param

--- server/src/public_api/controller.ts
+++ server/src/public_api/controller.ts
@@ -5,32 +5,32 @@ import queries from './queries.js';
 const getTicket = async (req: Request, res: Response) => {
   try {
     const limit = req.params.limit;
-    const offset = req.params.offset;
-    const tickets = await pool.query(queries.get_ticket, [limit, offset]);
-    res.status(200).json(tickets.rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
-  }
-};
-//What to do tmr merge these two functions into one and separate by the status param passed in
-const getTicketWithStatusFilter = async (req: Request, res: Response) => {
-  try {
-    const limit = req.params.limit;
     const status = req.params.status;
     const offset = req.params.offset;
 
-    if (!Number.isInteger(limit) || !Number.isInteger(status) || !Number.isInteger(offset)) {
-      res.status(400).send('Bad Request');
+    if (!limit && !offset) {
+      res.status(400).send('Bad Request, Required Limit and Offset');
       return;
     }
-
-    const tickets = await pool.query(queries.get_ticket_w_status_filter, [status, limit, offset]);
-    res.status(200).json(tickets.rows);
+    if (status) {
+      if (!Number.isInteger(limit) || !Number.isInteger(status) || !Number.isInteger(offset)) {
+        res.status(400).send('Bad Request');
+        return;
+      }
+      const tickets = await pool.query(queries.get_ticket_w_status_filter, [status, limit, offset]);
+      res.status(200).json(tickets.rows);
+    } else {
+      if (!Number.isInteger(limit) || !Number.isInteger(offset)) {
+        res.status(400).send('Bad Request');
+        return;
+      }
+      const tickets = await pool.query(queries.get_ticket, [limit, offset]);
+      res.status(200).json(tickets.rows);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send(error);
   }
 };
 
-export { getTicket, getTicketWithStatusFilter };
+export { getTicket };
